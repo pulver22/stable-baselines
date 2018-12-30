cc4405506b36426d594efdd3059d7507812981f1
Riccardo <pulver22@gmail.com>
2018-12-30 10:26:11 +0100

    Define new class NavigationCnnPolicy

--- stable_baselines/common/policies.py
+++ stable_baselines/common/policies.py
@@ -400,8 +400,25 @@ class CnnPolicy(FeedForwardPolicy):
 
     def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse=False, **_kwargs):
         super(CnnPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse,
-                                        feature_extraction="navigation_cnn", **_kwargs)
+                                        feature_extraction="cnn", **_kwargs)
 
+class NavigationCnnPolicy(FeedForwardPolicy):
+    """
+    Policy object that implements actor critic, using a CNN (the nature CNN)
+
+    :param sess: (TensorFlow session) The current TensorFlow session
+    :param ob_space: (Gym Space) The observation space of the environment
+    :param ac_space: (Gym Space) The action space of the environment
+    :param n_env: (int) The number of environments to run
+    :param n_steps: (int) The number of steps to run for each environment
+    :param n_batch: (int) The number of batch to run (n_envs * n_steps)
+    :param reuse: (bool) If the policy is reusable or not
+    :param _kwargs: (dict) Extra keyword arguments for the nature CNN feature extraction
+    """
+
+    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse=False, **_kwargs):
+        super(NavigationCnnPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse,
+                                        feature_extraction="navigation_cnn", **_kwargs)
 
 class CnnLstmPolicy(LstmPolicy):
     """
